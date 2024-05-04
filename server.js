779e0c9f5a8a849705337f3e02d765563ebf8778
Grittamet Wilai <132270499+grittametw@users.noreply.github.com>
2024-05-05 02:06:47 +0700

    Finished

--- server.js
+++ server.js
@@ -114,9 +114,9 @@ app.get('/:id_users', function (req, res, next) {
         
                     // Retrieve color values ​​R, G, B from database
                     const skinColorRGB = { 
-                        R: results[0].color_r, 
-                        G: results[0].color_g, 
-                        B: results[0].color_b 
+                        R: results[0].R, 
+                        G: results[0].G, 
+                        B: results[0].B 
                     };
         
                     // All tone colors
